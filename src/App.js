9ddc35cbdb3c8cc2404eea5a7047f638d059168f
Dalton Phillips <daltonphillips1020@gmail.com>
2022-01-03 16:35:13 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
+import contract from "./contracts/PsychPunks.json";
+
+const contractAddress = "0x26582b17C733bBC60FA41c833c9D814FC9CC4B03";
+const abi = contract.abi;
 
 function App() {
-  return <h1>Hello World</h1>;
+  return <h1>Hello world</h1>;
 }
 
 export default App;
